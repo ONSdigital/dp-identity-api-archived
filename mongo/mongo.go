93abea0b11b1a272b487e6e73ba30b3fc4b2e3e5
mikeAdams <mikelovesbooks@gmail.com>
2018-08-28 17:08:16 +0100

    Updated with storer interface

--- mongo/mongo.go
+++ mongo/mongo.go
@@ -5,6 +5,8 @@ import (
 	"time"
 
 	"errors"
+	"github.com/ONSdigital/dp-identity-api/models"
+	"github.com/globalsign/mgo/bson"
 )
 
 // Mongo represents a simplistic MongoDB configuration.
@@ -30,4 +32,39 @@ func (m *Mongo) Init() (session *mgo.Session, err error) {
 	session.EnsureSafe(&mgo.Safe{WMode: "majority"})
 	session.SetMode(mgo.Strong, true)
 	return session, nil
+}
+
+// GetIdentity gets an identity document
+func (store *Mongo) GetIdentity(id string) (*models.Identity, error) {
+
+	s := store.Session.Copy()
+	defer s.Close()
+	var identity models.Identity
+	err := s.DB(store.Database).C("identities").Find(bson.M{"_id": id}).One(&identity)
+	if err != nil {
+		if err == mgo.ErrNotFound {
+			return nil, errors.New("identity not found")
+		}
+		return nil, err
+	}
+
+	return &identity, nil
+}
+
+// CreateIdentity creates an identty document
+func (store *Mongo) CreateIdentity(identity *models.Identity) error {
+
+	s := store.Session.Copy()
+	defer s.Close()
+
+	err := s.DB(store.Database).C("identities").Insert(identity)
+	if err == mgo.ErrNotFound {
+		return errors.New("Failed to post identity to mongo")
+	}
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- store/store.go
+++ store/store.go
@@ -1,48 +1,13 @@
 package store
 
-import (
-	"github.com/ONSdigital/dp-identity-api/models"
-	"github.com/ONSdigital/dp-identity-api/mongo"
-	"github.com/globalsign/mgo"
-	"github.com/globalsign/mgo/bson"
-	"github.com/pkg/errors"
-)
+import "github.com/ONSdigital/dp-identity-api/models"
 
 type DataStore struct {
-	Backend mongo.Mongo
+	Backend Storer
 }
 
-
-func (store *DataStore) GetIdentity(id string) (*models.Identity, error) {
-
-	s := store.Backend.Session.Copy()
-	defer s.Close()
-	var identity models.Identity
-	err := s.DB(store.Backend.Database).C("identities").Find(bson.M{"_id": id}).One(&identity)
-	if err != nil {
-		if err == mgo.ErrNotFound {
-			return nil, errors.New("identity not found")
-		}
-		return nil, err
-	}
-
-	return &identity, nil
-}
-
-
-func (store *DataStore) CreateIdentity(identity *models.Identity) error {
-
-	s := store.Backend.Session.Copy()
-	defer s.Close()
-
-	err := s.DB(store.Backend.Database).C("identities").Insert(identity)
-	if err == mgo.ErrNotFound {
-		return errors.New("Failed to post identity to mongo")
-	}
-
-	if err != nil {
-		return err
-	}
-
-	return nil
+// Storer represents basic data access
+type Storer interface {
+	GetIdentity(id string) (*models.Identity, error)
+	CreateIdentity(identity *models.Identity) error
 }
