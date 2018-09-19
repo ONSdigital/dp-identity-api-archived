e63889672457e85b04cf9d1f1dcc30cc89407097
Dave Llewellyn <439837@gmail.com>
2018-09-19 12:45:36 +0100

    added new createdDate field

--- schema/schema.go
+++ schema/schema.go
@@ -1,5 +1,7 @@
 package schema
 
+import "time"
+
 var (
 	ErrIdentityNil        = ValidationErr{message: "identity required but was nil"}
 	ErrNameValidation     = ValidationErr{message: "mandatory field name was empty"}
@@ -18,14 +20,15 @@ func (e ValidationErr) Error() string {
 
 //Identity is an object representation of a user identity.
 type Identity struct {
-	ID                string `bson:"id" json:"id"`
-	Name              string `bson:"name" json:"name"`
-	Email             string `bson:"email" json:"email"`
-	Password          string `bson:"password" json:"password"`
-	UserType          string `bson:"user_type" json:"user_type"`
-	TemporaryPassword bool   `bson:"temporary_password" json:"temporary_password"`
-	Migrated          bool   `bson:"migrated" json:"migrated"`
-	Deleted           bool   `bson:"deleted" json:"deleted"`
+	ID                string    `bson:"id" json:"id"`
+	Name              string    `bson:"name" json:"name"`
+	Email             string    `bson:"email" json:"email"`
+	Password          string    `bson:"password" json:"password"`
+	UserType          string    `bson:"user_type" json:"user_type"`
+	TemporaryPassword bool      `bson:"temporary_password" json:"temporary_password"`
+	Migrated          bool      `bson:"migrated" json:"migrated"`
+	Deleted           bool      `bson:"deleted" json:"deleted"`
+	CreatedDate       time.Time `bson:"createdDate" json:"createdDate"`
 }
 
 func (i *Identity) Validate() (err error) {
