f05b47f046ecdb4929e1f725994c6f1b9838679e
mikeAdams <mikelovesbooks@gmail.com>
2018-10-02 15:51:42 +0100

    improved a comment

--- token/models.go
+++ token/models.go
@@ -2,7 +2,7 @@ package token
 
 import "github.com/ONSdigital/dp-identity-api/persistence"
 
-//Service encapsulates the logic for creating, updating and deleting tokens
+//Service encapsulates the logic for storing and getting tokens
 type Service struct {
 	persistence.TokenStore
 }
