bb2d89616aa03c3c6230d6b9c0ef5f70dff0cb2f
Dave Llewellyn <439837@gmail.com>
2018-10-03 08:12:01 +0100

    refined token store

--- mongo/token_store.go
+++ mongo/token_store.go
@@ -13,24 +13,16 @@ const (
 	identityIDKey = "identity_id"
 )
 
-// StoreToken create a new token document in mongodb tokens collection. Any active token associated with the identity
+// StoreToken store a new token document in mongodb tokens collection. Any active token associated with the identity
 // will be marked as deleted. Sets the last modified date on all documents updated.
 func (m *Mongo) StoreToken(ctx context.Context, tkn schema.Token, i schema.Identity, ttl time.Duration) error {
 	logD := log.Data{identityIDKey: i.ID}
 	log.InfoCtx(ctx, "tokenStore: storing identity token", logD)
 
-	active, err := m.findActiveTokens(ctx, i)
-	if err != nil {
-		return errors.Wrap(err, "tokenStore findActiveTokens return an error")
-	}
-
-	err = m.deleteActiveTokens(ctx, i, active)
+	_, err := m.deleteTokens(ctx, i.ID)
 	if err != nil {
 		return err
 	}
-
-	s := m.Session.Copy()
-	defer s.Close()
 
 	err = m.storeNewActiveToken(ctx, tkn)
 	if err != nil {
@@ -40,55 +32,40 @@ func (m *Mongo) StoreToken(ctx context.Context, tkn schema.Token, i schema.Ident
 }
 
 func (m *Mongo) GetToken(ctx context.Context, tokenStr string) (time.Duration, error) {
+	// TODO
 	return time.Second * 0, nil
 }
 
-func (m *Mongo) findActiveTokens(ctx context.Context, i schema.Identity) ([]schema.Token, error) {
-	log.InfoCtx(ctx, "tokenStore: querying for active tokens", log.Data{identityIDKey: i.ID})
-	s := m.Session.Copy()
-	defer s.Close()
-
-	query := bson.M{"identity_id": i.ID, "deleted": false}
-
-	var active []schema.Token
-	err := s.DB(m.Database).C(m.TokenCollection).Find(query).All(&active)
-	if err != nil {
-		return nil, errors.Wrap(err, "tokenStore: query for active tokens returned an error")
-	}
-	return active, nil
-}
-
-func (m *Mongo) deleteActiveTokens(ctx context.Context, i schema.Identity, active []schema.Token) error {
-	logD := log.Data{identityIDKey: i.ID}
-
-	if len(active) == 0 {
-		log.InfoCtx(ctx, "tokenStore: no currently active tokens to delete for identity", logD)
-		return nil
-	}
-
+// deleteTokens soft delete any active token associated with the provided identity ID. Sets token.deleted = true and
+// updates token.last_modified to the current time. Returns the number of documents updated or any error encountered
+// while executing.
+func (m *Mongo) deleteTokens(ctx context.Context, identityID string) (int, error) {
+	logD := log.Data{identityIDKey: identityID}
 	log.InfoCtx(ctx, "tokenStore: deleting active token(s) for identity", logD)
 
 	s := m.Session.Copy()
 	defer s.Close()
 
-	selector := bson.M{"identity_id": i.ID}
+	selector := bson.M{"identity_id": identityID, "deleted": false}
 	update := bson.M{"$set": bson.M{"deleted": true, "last_modified": time.Now()}}
 
 	info, err := s.DB(m.Database).C(m.TokenCollection).UpdateAll(selector, update)
 	if err != nil {
-		return errors.Wrap(err, "tokenStore: error deleting active token(s) for identity")
+		return 0, errors.Wrap(err, "tokenStore: error deleting active token(s) for identity")
 	}
 
 	log.InfoCtx(ctx, "tokenStore: delete active tokens completed without error", log.Data{
-		"identity_id": i.ID,
+		"identity_id": identityID,
 		"changeInfo": changeInfo{
 			"matched": info.Matched,
 			"updated": info.Updated,
 		},
 	})
-	return nil
+	return info.Updated, nil
 }
 
+// storeNewActiveToken store the provided token in the Tokens collection. Token will become the active token for this
+// identity.
 func (m *Mongo) storeNewActiveToken(ctx context.Context, tkn schema.Token) error {
 	s := m.Session.Copy()
 	defer s.Close()
@@ -97,6 +74,7 @@ func (m *Mongo) storeNewActiveToken(ctx context.Context, tkn schema.Token) error
 	log.InfoCtx(ctx, "tokenStore: storing new active identity token", logD)
 
 	tkn.LastModified = time.Now()
+	tkn.Deleted = false // always set to false to ensure this is now the active token.
 
 	err := s.DB(m.Database).C(m.TokenCollection).Insert(tkn)
 	if err != nil {
@@ -105,4 +83,21 @@ func (m *Mongo) storeNewActiveToken(ctx context.Context, tkn schema.Token) error
 
 	log.InfoCtx(ctx, "tokenStore: store new active identity token successful", logD)
 	return nil
+}
+
+// activeTokens return a list of tokens associated with the provided identity with "deleted = false".
+func (m *Mongo) getActiveTokens(ctx context.Context, identityID string) ([]schema.Token, error) {
+	log.InfoCtx(ctx, "tokenStore: querying for active tokens", log.Data{identityIDKey: identityID})
+	s := m.Session.Copy()
+	defer s.Close()
+
+	query := bson.M{"identity_id": identityID, "deleted": false}
+
+	var active []schema.Token
+	err := s.DB(m.Database).C(m.TokenCollection).Find(query).All(&active)
+	if err != nil {
+		return nil, errors.Wrap(err, "tokenStore: query for active tokens returned an error")
+	}
+
+	return active, nil
 }
